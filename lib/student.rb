dddc3f5ad7e93b7a2077e6febb998ae92416a016
Jay Bednar <ironviralmedia@gmail.com>
2018-03-14 17:27:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,4 +98,15 @@ class Student
     SQL
     self.new_from_db(DB[:conn].execute(sql).first)
   end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?;
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
